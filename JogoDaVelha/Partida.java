3e47d2b2e233120e102e8f744601362dae1f87a1
Lucas Jacobi <lukas_jacoby@hotmail.com>
2018-03-20 20:48:50 -0300

    Segundo commit

--- JogoDaVelha/Partida.java
+++ JogoDaVelha/Partida.java
@@ -1,15 +1,41 @@
-import java.util.Scanner; //você pode utilizar o Scanner para interagir com o usuário, perguntando o nome dos jogadores antes de iniciar a partida
-import java.util.Random; //pode usar o random para sortear quem inicia, jogador 1 ou o 2;
+import java.util.Scanner; 
+import java.util.Random; 
 
 public class Partida {
-	/*
-	* você pode ter uma variável para controlar a vez de quem joga 1 ou 2 e alternar seu valor até o fim do jogo
-	* você precisa de um tabuleiro para iniciar uma partida
-	* também se faz necessário 2 jogadores, não é mesmo?
-	*/
+
+	private int quemJogando;
+	private Tabuleiro tabuleiro;
+	private Jogador jogador_1;
+	private Jogador jogador_2;
+	private int whoStarts;
 	
 	public Partida() {
-		// Aqui é o construtor, quando a partida for construida, você precisa definir quem está jogando além de fazer o necessário para rodar o jogo.
+		// Aqui é o construtor, quando a partida for construida, você precisa definir quem está jogando
+		// além de fazer o necessário para rodar o jogo.
+
+		String nome;
+		String tipo;
+
+		Scanner scan = new Scanner(System.in);
+		Random random = new Random();
+		whoStarts = random.nextInt(99);
+		if (whoStarts % 2 == 0) {
+			whoStarts = 2;
+		} else {
+			whoStarts = 1;
+		}
+
+		this.jogador_1 = new Jogador(nome, tipo);
+		this.jogador_2 = new Jogador(nome, tipo);
+
+		
+		System.out.println("*--- Jogo da velha ---*");
+		System.out.println("Digite o nome do primeiro jogador: ");
+		nome = scan.next();
+
+		while () {
+			System.out.println("Digite o tipo do primeiro jogador: ");
+		}	
 	}
 
 	public void play() {
